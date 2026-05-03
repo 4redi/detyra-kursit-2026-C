8bd96c823d9a6ff4654c2fcd520f8f48c30951c4
Redi <redicurrii@yahoo.com>
2026-05-03 19:41:22 +0200

    Handled username, fixed logical login error for user handling, implemented user menu

--- main.c
+++ main.c
@@ -9,10 +9,11 @@
 // ###### Menuja #######
 
 void menuja();
-int login(int *id_user);
+int login(int *id_user, char username[]);
 void regjistroPerdorues();
 
 // ###### Fundi menuja #######
+
 
 // ####### Administator funksionet ########
 
@@ -31,9 +32,9 @@ void statistika();        // si tip menuje ku mund te shohesh nr users, total sh
 
 // ######## Perdorues funksionet ##########
 
-void userMenu(int id_user); // vendi ku mbahen te gjitha funx. e meposhthme
-void shtoShpenzim();
-void shtoTeArdhura();
+void userMenu(int id_user, char username[]); // vendi ku mbahen te gjitha funx. e meposhthme
+void shtoShpenzim(int id_user);
+void shtoTeArdhura(int id_user);
 void shfaqShpenzime(int id_user);
 void shfaqTeArdhura(int id_user);
 void raportMujor(int id_user);
@@ -175,11 +176,11 @@ int eshtIdValid(int id_user)
             return 0; // id jo unike
         }
     }
+    fclose(fptr);
     return 1; // id unike sepse nuk u plotesua kushti ndalues (testimi i jo unicitetit)
 }
 
 // * ######## Fundi Funx. Handler ########
-
 
 // * ###### Fillimi i Menuve ####### //
 void menuja()
@@ -187,53 +188,171 @@ void menuja()
     int choice;
     int id_user;
     int role;
-    do{
-printf("\n####### Mire se vini #######\n");
-    printf("1. Administrator\n");
-    printf("2. Perdorues\n");
-    printf("3. Dil\n");
-    printf("Zgjidhni: ");
-    scanf("%d", &choice);
-
-    switch (choice)
+    char username[20];
+    do
     {
-    case 1:
-        role = login(&id_user);
+        printf("\n####### Mire se vini #######\n");
+        printf("1. Administrator\n");
+        printf("2. Perdorues\n");
+        printf("3. Dil\n");
+        printf("Zgjidhni: ");
+        scanf("%d", &choice);
 
-        if (role == LOGIN_ADMIN)
+        switch (choice)
         {
-            adminMenu();
+        case 1:
+            role = login(&id_user, username);
+
+            if (role == LOGIN_ADMIN)
+            {
+                adminMenu();
+            }
+            else if (role == LOGIN_FAIL)
+            {
+                printf("Login deshtoi!\n");
+            }
+            break;
+
+        case 2:
+            role = login(&id_user, username);
+
+            if (role == LOGIN_USER)
+            {
+                userMenu(id_user, username);
+            }
+
+            break;
+
+        case 3:
+            return;
+
+        default:
+            printf("Invalid!\n");
         }
-        else if (role == LOGIN_FAIL)
-        {
-            printf("Login deshtoi!\n");
-        }
-        break;
-
-    case 2:
-        role = login(&id_user);
-
-        if (role == LOGIN_USER)
-        {
-            userMenu(id_user);
-        }
-
-        break;
-
-    case 3:
-        return;
-
-    default:
-        printf("Invalid!\n");
-    }
-    }while(choice!=3);
+    } while (choice != 3);
 }
 
-
-
-void userMenu(int id_user)
+void userMenu(int id_user, char username[])
 {
-    printf("Mireserdhet user %d", id_user);
+    printf("\n\n");
+    printf("------------------- Mire se erdhet perdorues:%s me ID:%d  -----------------------------\n", username, id_user);
+    int choice;
+    do
+    {
+        printf("1.Shto nje shpenzim te ri\n");
+        printf("2.Shto nje te ardhur te re\n");
+        printf("3.Shfaq shpenzimet e mia\n");
+        printf("4.Shfaq te ardhurat e mia\n");
+        printf("5.Raport Mujor\n");
+        printf("6.Raport Vjetor\n");
+        printf("7.Kontrollo Buxhetin\n");
+        printf("8.Kerko shpenzim sipas kategorise\n");
+        printf("9.Fshini te dhenat e nje shpenzimi\n");
+        printf("10.Rendit shpenzimet sipas shumes\n");
+        printf("11.Rendit shpenzimet sipas dates\n");
+        printf("12.Kerko shpenzim sipas ID\n");
+        printf("13.Kerko shpenzim sipas dates\n");
+        printf("14.Kerko shpenzim sipas nje intervali datash\n");
+        printf("15.Dil\n");
+
+        printf("Beni zgjedhjen tuaj:");
+        scanf("%d", &choice);
+        printf("\n\n");
+
+        switch (choice)
+        {
+        case 1:
+            shtoShpenzim(id_user);
+            break;
+
+        case 2:
+            shtoTeArdhura(id_user);
+            break;
+
+        case 3:
+            shfaqShpenzime(id_user);
+            break;
+
+        case 4:
+            shfaqTeArdhura(id_user);
+            break;
+
+        case 5:
+            raportMujor(id_user);
+            break;
+
+        case 6:
+            raportVjetor(id_user);
+            break;
+
+        case 7:
+            kontrolloBuxhetin(id_user);
+            break;
+
+        case 8:
+        {
+            int id_kategori;
+            printf("Vendos ID e kategorise: ");
+            scanf("%d", &id_kategori);
+            kerkoShpenzimSipasKategorise(id_user, id_kategori);
+            break;
+        }
+
+        case 9:
+        {
+            int id_shpenzim;
+            printf("Vendos ID e shpenzimit per fshirje: ");
+            scanf("%d", &id_shpenzim);
+            fshiShpenzim(id_shpenzim);
+            break;
+        }
+
+        case 10:
+            renditShpenzimeSipasShumes(id_user);
+            break;
+
+        case 11:
+            renditShpenzimeSipasDates(id_user);
+            break;
+
+        case 12:
+        {
+            int id_shpenzim;
+            printf("Vendos ID e shpenzimit: ");
+            scanf("%d", &id_shpenzim);
+            kerkoShpenzimSipasId(id_shpenzim);
+            break;
+        }
+
+        case 13:
+        {
+            char data[20];
+            printf("Vendos daten (p.sh. 2026-05-03): ");
+            scanf("%s", data);
+            kerkoShpenzimSipasDate(id_user, data);
+            break;
+        }
+
+        case 14:
+        {
+            char data1[20], data2[20];
+            printf("Vendos daten e pare: ");
+            scanf("%s", data1);
+            printf("Vendos daten e dyte: ");
+            scanf("%s", data2);
+            kerkoShpenzimeIntervalDate(id_user, data1, data2);
+            break;
+        }
+
+        case 15:
+            menuja();
+            break;
+
+        default:
+            printf("Zgjedhje e pavlefshme!\n");
+        }
+
+    } while (choice != 15);
 }
 
 void adminMenu()
@@ -242,8 +361,6 @@ void adminMenu()
 }
 
 // * ###### Fundi i Menuve ####### //
-
-
 
 // * ###### Regjistrohu dhe Logohu ######  //
 void regjistroPerdorues()
@@ -321,21 +438,21 @@ void regjistroPerdorues()
     printf("Perdoruesi u regjistrua me sukses!\n");
 }
 
-int login(int *id_user)
+int login(int *id_user, char username[])
 {
     FILE *fptr = fopen("user.txt", "r");
 
-    char username[20], password[50];
+    char inputUsername[20], password[50];
 
     printf("Username: ");
-    scanf("%s", username);
+    scanf("%s", inputUsername);
 
     printf("Password: ");
     scanf("%s", password);
 
-    // ADMIN
-    if (strcmp(username, "admin") == 0 && strcmp(password, "Admin123") == 0)
+    if (strcmp(inputUsername, "admin") == 0 && strcmp(password, "Admin123") == 0)
     {
+        strcpy(username, "admin");
         return LOGIN_ADMIN;
     }
 
@@ -349,55 +466,94 @@ int login(int *id_user)
     char emri[30], fileUsername[20], filePassword[50];
     float buxheti;
 
-    int foundUser = 0;
-
     while (fscanf(fptr, "%d %s %s %s %f",
                   &fileId, emri, fileUsername, filePassword, &buxheti) != EOF)
     {
-
-        if (strcmp(username, fileUsername) == 0)
+        if (strcmp(inputUsername, fileUsername) == 0)
         {
-            foundUser = 1;
-
             if (strcmp(password, filePassword) == 0)
             {
                 *id_user = fileId;
+                strcpy(username, fileUsername);
                 fclose(fptr);
                 return LOGIN_USER;
+            }
+            else
+            {
+                printf("Password gabim!\n");
+                fclose(fptr);
+                return LOGIN_FAIL;
             }
         }
     }
 
     fclose(fptr);
-    char choice;
-    // ! User exists por, pw gabim
-    if (foundUser)
-    {
-        printf("Username i sakte por password gabim!\n");
-    }
-    // ! User nuk ekziston ne file
-    else
-    {
-        printf("User-i nuk ekziston!\n");
-    }
-
-    printf("Deshiron te regjistrosh user tjeter? (p/j): ");
-    scanf(" %c", &choice);
-
-    if (choice == 'p' || choice == 'P')
-    {
-        regjistroPerdorues();
-    }
-
-    scanf(" %c", &choice);
-
-    if (choice == 'p' || choice == 'P')
-    {
-        regjistroPerdorues();
-    }
-
+    printf("User-i nuk ekziston!\n");
     return LOGIN_FAIL;
 }
 
 // * ###### Regjistrohu dhe Logohu Fundi ######  //
 // TODO: Handle Menu-te, funksionet e perdoruesit
+
+
+
+// * ###### User functions ########
+
+
+void shtoShpenzim(int id_user) {
+    printf("shtoShpenzim not implemented yet\n\n");
+}
+
+void shtoTeArdhura(int id_user) {
+    printf("shtoTeArdhura not implemented yet\n\n");
+}
+
+void shfaqShpenzime(int id_user) {
+    printf("shfaqShpenzime not implemented yet\n\n");
+}
+
+void shfaqTeArdhura(int id_user) {
+    printf("shfaqTeArdhura not implemented yet\n\n");
+}
+
+void raportMujor(int id_user) {
+    printf("raportMujor not implemented yet\n\n");
+}
+
+void raportVjetor(int id_user) {
+    printf("raportVjetor not implemented yet\n\n");
+}
+
+void kontrolloBuxhetin(int id_user) {
+    printf("kontrolloBuxhetin not implemented yet\n\n");
+}
+
+void kerkoShpenzimSipasKategorise(int id_user, int id_kategori) {
+    printf("kerkoShpenzimSipasKategorise not implemented yet\n\n");
+}
+
+void fshiShpenzim(int id_shpenzim) {
+    printf("fshiShpenzim not implemented yet\n\n");
+}
+
+void renditShpenzimeSipasShumes(int id_user) {
+    printf("renditShpenzimeSipasShumes not implemented yet\n\n");
+}
+
+void renditShpenzimeSipasDates(int id_user) {
+    printf("renditShpenzimeSipasDates not implemented yet\n\n");
+}
+
+void kerkoShpenzimSipasId(int id_shpenzim) {
+    printf("kerkoShpenzimSipasId not implemented yet\n\n");
+}
+
+void kerkoShpenzimSipasDate(int id_user, char data[]) {
+    printf("kerkoShpenzimSipasDate not implemented yet\n\n");
+}
+
+void kerkoShpenzimeIntervalDate(int id_user, char data1[], char data2[]) {
+    printf("kerkoShpenzimeIntervalDate not implemented yet\n\n");
+}
+
+// * ##### endregion
