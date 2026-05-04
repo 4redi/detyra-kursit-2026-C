61d3ccefb55ef4cc69aa2b25e729b496bd3067cf
Redi <redicurrii@yahoo.com>
2026-05-04 19:45:45 +0200

    Add admin menu implementation and header file for admin functions

---
+++ admin.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+#include "admin.h"
+
+
+void adminMenu()
+{
+    printf("Hi lol admin!\n");
+}
---
+++ admin.h
@@ -0,0 +1,17 @@
+#ifndef ADMIN_H
+#define ADMIN_H
+
+void adminMenu();
+// ####### Administator funksionet ########
+
+void adminMenu(); // vendi ku do mbahen te gjitha funx. e meposhtme
+void shtoPerdorues();
+void shfaqPerdoruesit();
+void kerkoSipasId(int id_user);
+void fshiPerdorues(int id_user);
+void updatePerdorues(int id_user);
+void kerkoSipasUsername(char username[]);
+void rradhitSipasUsername();
+void adminMenuKategori(); // crud per kategorine
+void statistika();        // si tip menuje ku mund te shohesh nr users, total shpenzimesh, mesatare shpenzimesh, kategoria max, etc
+#endif
