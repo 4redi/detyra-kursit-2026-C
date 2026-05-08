412d40ab28ee1a88364f0c994f59ad22308f8aa4
Redi <redicurrii@yahoo.com>
2026-05-08 19:48:41 +0200

    New handler functions, too lazy to name

--- utils.h
+++ utils.h
@@ -1,8 +1,14 @@
 #ifndef UTILS_H
 #define UTILS_H
 
-int eshtIdValid(int id_user);
+int gjeneroIdUser();
 int eshtUsernameUnik(const char username[]);
 int eshtPasswordValid(const char password[]);
-
+int gjeneroIdShpenzim();
+int lexoDaten(int *dita, int *muaji, int *viti);
+int eshtVitiBisht(int viti);
+int diteNeMuaj(int muaji, int viti);
+float totalTeArdhura(int id_user);
+float totalShpenzime(int id_user);
+int gjeneroIdArdhure();
 #endif
