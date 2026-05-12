05097c566c4263611bc765383016a1c264010c04
Redi <redicurrii@yahoo.com>
2026-05-12 19:40:34 +0200

    Enhance user management: add total expenses, total income, and balance calculations; implement user deletion and update functionalities with file handling

--- admin.c
+++ admin.c
@@ -150,6 +150,9 @@ void kerkoSipasId(int id_user){
             printf("Username : %s\n", p.username);
             printf("Password : %s\n", p.password);
             printf("Buxheti  : %.2f\n", p.buxheti_vjetor);
+            printf("Shpenzime Totale  : %.2f\n",totalShpenzime(p.id_user));
+            printf("Ardhura Totale  : %.2f\n",totalTeArdhura(p.id_user));\
+            printf("Mbetja  : %.2f\n",totalTeArdhura(p.id_user)-totalShpenzime(p.id_user));
             found = 1;
             break;
         }
@@ -172,6 +175,8 @@ void fshiPerdorues(int id_user){
     }
 
     struct Perdorues p;
+    struct Shpenzim s;
+    struct Te_Ardhura t;
     int found = 0;
 
     while(fscanf(fptr, "%d %s %s %s %f",
@@ -206,25 +211,248 @@ void fshiPerdorues(int id_user){
     }
 
     remove("user.txt");
+     rename("users_temp.txt", "user.txt");
 
-    if(rename("users_temp.txt", "user.txt") != 0){
-        char buf[256];
-        strerror_s(buf,256,errno);
-        printf("Error:%s",buf);
-        return;
+
+    // sjpenzimet
+    FILE *fs=fopen("shpenzime.txt","r");
+    FILE *tempS=fopen("temp_shpenzime.txt","w");
+
+    if (fs != NULL && tempS != NULL)
+    {
+        while (fscanf(fs, "%d %d %s %d %s %s %f %s",
+                      &s.id_shpenzim,
+                      &s.id_user,
+                      s.pershkrim,
+                      &s.kategori.id_kategoria,
+                      s.kategori.emertimi,
+                      s.kategori.pershkrimi,
+                      &s.shuma,
+                      s.data) == 8)
+        {
+            if (s.id_user == id_user)
+                continue;
+
+            fprintf(tempS, "%d %d %s %d %s %s %.2f %s\n",
+                    s.id_shpenzim,
+                    s.id_user,
+                    s.pershkrim,
+                    s.kategori.id_kategoria,
+                    s.kategori.emertimi,
+                    s.kategori.pershkrimi,
+                    s.shuma,
+                    s.data);
+        }
+
+        fclose(fs);
+        fclose(tempS);
+
+        remove("shpenzime.txt");
+        rename("temp_shpenzime.txt", "shpenzime.txt");
     }
+
+    // t ardhura
+
+    FILE *fa = fopen("teArdhura.txt", "r");
+    FILE *tempA = fopen("temp_ardhura.txt", "w");
+
+    if (fa != NULL && tempA != NULL)
+    {
+        while (fscanf(fa, "%d %d %s %f %s",
+                      &t.id_burimi,
+                      &t.id_user,
+                      t.burimi,
+                      &t.shuma,
+                      t.data) == 5)
+        {
+            if (t.id_user == id_user)
+                continue;
+
+            fprintf(tempA, "%d %d %s %.2f %s\n",
+                    t.id_burimi,
+                    t.id_user,
+                    t.burimi,
+                    t.shuma,
+                    t.data);
+        }
+
+        fclose(fa);
+        fclose(tempA);
+
+        remove("teArdhura.txt");
+        rename("temp_ardhura.txt", "teArdhura.txt");
+    }
+
+    printf("Perdoruesi dhe te dhenat e tij u fshine me sukses!\n");
 
     printf("Perdoruesi u fshi me sukses!\n");
 }
 
 void updatePerdorues(int id_user){
-    printf("Akoma");
+    FILE *fptr=fopen("user.txt","r");
+    FILE *temp=fopen("temp.txt","w");
+
+    if(fptr==NULL||temp==NULL){
+        printf("Gabim ne hapjen e file-ve");
+        return;
+    }
+
+    struct Perdorues p;
+    int found=0;
+    char newUsername[20];
+    while(fscanf(fptr,"%d %s %s %s %f",&p.id_user,p.emri,p.username,p.password,&p.buxheti_vjetor)==5){
+        if(p.id_user==id_user){
+            found=1;
+            printf("Username aktual: %s\n", p.username);
+
+            do{
+                printf("Vendos nje username te ri:");
+                scanf("%s",newUsername);
+                if (!eshtUsernameUnik(newUsername))
+                {
+                    printf("Username ekziston!\n");
+                }
+                else if (strlen(newUsername) < 5)
+                {
+                    printf("Username duhet te kete te pakten 5 karaktere!\n");
+                }
+            }while(!eshtUsernameUnik(newUsername));
+            strcpy(p.username,newUsername);
+        }
+
+        fprintf(temp, "%d %s %s %s %.2f\n",
+                p.id_user,
+                p.emri,
+                p.username,
+                p.password,
+                p.buxheti_vjetor);
+    }
+    fclose(fptr);
+    fclose(temp);
+    fcloseall();
+
+    if (!found) {
+        remove("temp.txt");
+        printf("Perdoruesi nuk u gjet!\n");
+        return;
+    }
+
+    remove("user.txt");
+    rename("temp.txt", "user.txt");
+
+    printf("Username u ndryshua me sukses!\n");
 }
 void kerkoSipasUsername(char username[]){
-    printf("Akoma");
+    FILE *fptr = fopen("user.txt", "r");
+
+    if (fptr == NULL) {
+        printf("Gabim ne hapjen e file!\n");
+        return;
+    }
+
+    struct Perdorues p;
+    int found = 0;
+
+    while (fscanf(fptr, "%d %s %s %s %f",
+                  &p.id_user,
+                  p.emri,
+                  p.username,
+                  p.password,
+                  &p.buxheti_vjetor) == 5)
+    {
+        if (_stricmp(username, p.username) == 0)
+        {
+            found = 1;
+            break;
+        }
+    }
+
+    fclose(fptr);
+
+    if (!found) {
+        printf("Perdoruesi nuk u gjet!\n");
+        return;
+    }
+
+    int choice;
+
+    do {
+
+        printf("\n===== USER MENU =====\n");
+        printf("Username : %s\n", p.username);
+
+        printf("1. Info personale\n");
+        printf("2. Total shpenzime\n");
+        printf("3. Total te ardhura\n");
+        printf("4. Bilanci\n");
+        printf("5. Kthehu\n");
+
+        printf("Zgjedhja: ");
+        scanf("%d", &choice);
+
+        switch(choice)
+        {
+            case 1:
+                printf("\n===== INFO =====\n");
+                printf("ID         : %d\n", p.id_user);
+                printf("Emri       : %s\n", p.emri);
+                printf("Username   : %s\n", p.username);
+                printf("Buxheti    : %.2f\n", p.buxheti_vjetor);
+                break;
+            case 2:
+                printf("\nTotal shpenzime: %.2f\n",
+                totalShpenzime(p.id_user));
+                break;
+            case 3:
+                printf("\nTotal te ardhura: %.2f\n",
+                totalTeArdhura(p.id_user));
+                break;
+            case 4:
+            {
+                float ard = totalTeArdhura(p.id_user);
+                float shp = totalShpenzime(p.id_user);
+                printf("\nBilanci: %.2f\n", ard - shp);
+                break;
+            }
+            case 5:
+                break;
+            default:
+                printf("Zgjedhje e pavlefshme!\n");
+        }
+
+    } while(choice != 5);
 }
 void rradhitSipasUsername(){
-    printf("Akoma");
+    // mendoje si nje array dhe do i besh order me bubble sort
+
+    FILE *fptr=fopen("user.txt","r");
+    if(fptr==NULL){
+        printf("Dicka shkoi keq");
+        return;
+    }
+    struct Perdorues p[100];
+    int n=0; //iteruesja
+    while(fscanf(fptr,"%d %s %s %s %f",&p[n].id_user,p[n].emri,p[n].username,p[n].password,&p[n].buxheti_vjetor)==5){
+        n++;
+    }
+
+    fclose(fptr);
+
+    for (int i = 0; i < n - 1; i++) {
+        for (int j = 0; j < n - i - 1; j++) {
+            if (strcmp(p[j].username, p[j + 1].username) > 0) {
+                struct Perdorues temp = p[j];
+                p[j] = p[j + 1];
+                p[j + 1] = temp;
+            }
+        }
+    }
+
+    printf("User-at e afishuar :\n");
+    for(int i=0;i<n;i++){
+        printf("%s\n",p[i].username);
+    }
+
 }
 void adminMenuKategori(){
     printf("Akoma");
